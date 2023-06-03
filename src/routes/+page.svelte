52044cd6b71dac4b29da93aeeca2046ee1836a6e
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-06-03 13:20:17 +0200

    Add initial implementation of buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { page } from '$app/stores'
+
+    $: gameState = $page.url.searchParams.get('gameState') || 'x---------'
+    $: playerTurn = gameState[0] === 'x' ? 'x' : 'o'
+
+    function newGameState(squareNumer: number) {
+        // player turn is the first character
+        // then the gameState is sliced from the second character to the square number
+        // then the player turn is added
+        // then the gameState is sliced from the square number + 1 to the end
+        // then all of that is concatenated together
+        return (
+            (playerTurn === 'x' ? 'o' : 'x') +
+            gameState.slice(1, squareNumer) +
+            playerTurn +
+            gameState.slice(squareNumer + 1)
+        )
+    }
+</script>
+
+<form method="GET" on:submit|preventDefault>
+    {#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as squareNumber}
+        <button
+            disabled={gameState[squareNumber] !== '-'}
+            name="gameState"
+            on:click={() => (gameState = newGameState(squareNumber))}
+            value={newGameState(squareNumber)}>
+            <!-- TODO: add visually hidden language and make visual value be aria ignore with a span -->
+            {gameState[squareNumber]}
+        </button>
+    {/each}
+</form>
+{gameState}
+
+<style>
+    form {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 1rem;
+        width: 100%;
+        height: 100%;
+    }
+</style>
