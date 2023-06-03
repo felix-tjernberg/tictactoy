ef385886fbaf5727c80e67a86ec5e729d9be2268
Felix Tjernberg <felix.tjernberg@hotmail.se>
2023-06-03 15:41:40 +0200

    Add player names

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,18 @@
 <script lang="ts">
-    import { page } from '$app/stores'
-
-    $: gameState = $page.url.searchParams.get('gameState') || 'x---------'
-    $: playerTurn = gameState[0] === 'x' ? 'x' : 'o'
-
-    function newGameState(squareNumer: number) {
-        // player turn is the first character
-        // then the gameState is sliced from the second character to the square number
-        // then the player turn is added
-        // then the gameState is sliced from the square number + 1 to the end
-        // then all of that is concatenated together
-        return (
-            (playerTurn === 'x' ? 'o' : 'x') +
-            gameState.slice(1, squareNumer) +
-            playerTurn +
-            gameState.slice(squareNumer + 1)
-        )
-    }
+    export let form
 </script>
 
-<form method="GET" on:submit|preventDefault>
-    {#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as squareNumber}
-        <button
-            disabled={gameState[squareNumber] !== '-'}
-            name="gameState"
-            on:click={() => (gameState = newGameState(squareNumber))}
-            value={newGameState(squareNumber)}>
-            <!-- TODO: add visually hidden language and make visual value be aria ignore with a span -->
-            {gameState[squareNumber]}
-        </button>
-    {/each}
+<form method="POST">
+    {#if form?.error}
+        <p>{form.error}</p>
+    {/if}
+    <label>
+        player one:
+        <input type="text" name="playerOne" id="playerOne" value={form?.playerOne ?? ''} />
+    </label>
+    <label>
+        player two:
+        <input type="text" name="playerTwo" id="playerTwo" value={form?.playerTwo ?? ''} />
+    </label>
+    <button type="submit">Start game</button>
 </form>
-{gameState}
-
-<style>
-    form {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-gap: 1rem;
-        width: 100%;
-        height: 100%;
-    }
-</style>
